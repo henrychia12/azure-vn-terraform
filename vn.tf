9eae0fd834efcddc4f8620edae78d88bf5815f9b
henrychia12 <henry_chia21@hotmail.co.uk>
2019-06-27 12:37:28 +0000

    working vn with ssh public ip access

--- vn.tf
+++ vn.tf
@@ -136,7 +136,7 @@ resource "azurerm_virtual_machine" "myterraformvm" {
         disable_password_authentication = true
         ssh_keys {
             path     = "/home/azureuser/.ssh/authorized_keys"
-            key_data = "ssh-rsa AAAAB3Nz{snip}hwhqT9h"
+            key_data = file("~/.ssh/id_rsa.pub")
         }
     }
 
