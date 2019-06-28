249347f4af1bd24e59e4d3cb109f873d1e0dd316
henrychia12 <henry_chia21@hotmail.co.uk>
2019-06-28 10:14:49 +0000

    python server terraform file

--- pythonserver.tf
+++ pythonserver.tf
@@ -1,9 +1,9 @@
-resource "azurerm_public_ip" "jenkinsslavepip" {
-    name                         = "jenkinsSlavePublicIP"
+resource "azurerm_public_ip" "pythonserverpip" {
+    name                         = "pythonServerPublicIP"
     location                     = "${azurerm_resource_group.main.location}"
     resource_group_name          = "${azurerm_resource_group.main.name}"
     allocation_method            = "Dynamic"
-    domain_name_label            = "jenkinsslave-${formatdate("DDMMYYhhmmss", timestamp())}"
+    domain_name_label            = "pythonserver-${formatdate("DDMMYYhhmmss", timestamp())}"
 
     tags = {
         environment = "Terraform Demo"
@@ -11,8 +11,8 @@ resource "azurerm_public_ip" "jenkinsslavepip" {
 }
 
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "jenkinsslavensg" {
-    name                = "jenkinsSlaveNetworkSecurityGroup"
+resource "azurerm_network_security_group" "pythonservernsg" {
+    name                = "pythonServerNetworkSecurityGroup"
     location            = "${azurerm_resource_group.main.location}"
     resource_group_name = "${azurerm_resource_group.main.name}"
     
@@ -28,23 +28,35 @@ resource "azurerm_network_security_group" "jenkinsslavensg" {
         destination_address_prefix = "*"
     }
 
+    security_rule {
+        name                       = "PythonServer"
+        priority                   = 200
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "3000"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
     tags = {
         environment = "Terraform Demo"
     }
 }
 
 # Create network interface
-resource "azurerm_network_interface" "jenkinsslavenic" {
-    name                      = "jenkinsSlaveNIC"
+resource "azurerm_network_interface" "pythonservernic" {
+    name                      = "pythonServerNIC"
     location                  = "${azurerm_resource_group.main.location}"
     resource_group_name       = "${azurerm_resource_group.main.name}"
-    network_security_group_id = "${azurerm_network_security_group.jenkinsslavensg.id}"
+    network_security_group_id = "${azurerm_network_security_group.pythonservernsg.id}"
 
     ip_configuration {
-        name                          = "jenkinsSlaveNicConfiguration"
+        name                          = "pythonServerNicConfiguration"
         subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.jenkinsslavepip.id}"
+        public_ip_address_id          = "${azurerm_public_ip.pythonserverpip.id}"
     }
 
     tags = {
@@ -53,11 +65,11 @@ resource "azurerm_network_interface" "jenkinsslavenic" {
 }
 
 # Create virtual machine
-resource "azurerm_virtual_machine" "jenkinsslavevm" {
-    name                  = "jenkinsSlaveVM"
+resource "azurerm_virtual_machine" "pythonservervm" {
+    name                  = "pythonServerVM"
     location              = "${azurerm_resource_group.main.location}"
     resource_group_name   = "${azurerm_resource_group.main.name}"
-    network_interface_ids = ["${azurerm_network_interface.jenkinsslavenic.id}"]
+    network_interface_ids = ["${azurerm_network_interface.pythonservernic.id}"]
     vm_size               = "Standard_DS1_v2"
 
     storage_os_disk {
@@ -76,13 +88,13 @@ resource "azurerm_virtual_machine" "jenkinsslavevm" {
 
     os_profile {
         computer_name  = "hostname"
-        admin_username = "jenkinsslave"
+        admin_username = "pythonserver"
     }
 
     os_profile_linux_config {
         disable_password_authentication = true
         ssh_keys {
-            path     = "/home/jenkinsslave/.ssh/authorized_keys"
+            path     = "/home/pythonserver/.ssh/authorized_keys"
             key_data = file("~/.ssh/id_rsa.pub")
         }
     }
@@ -102,9 +114,9 @@ resource "azurerm_virtual_machine" "jenkinsslavevm" {
                  ]
         connection {
 	    type = "ssh"
-	    user = "jenkinsslave"
+	    user = "pythonserver"
 	    private_key = file("~/.ssh/id_rsa")
-	    host = "${azurerm_public_ip.jenkinsslavepip.fqdn}"
+	    host = "${azurerm_public_ip.pythonserverpip.fqdn}"
        }
     } 
 }
